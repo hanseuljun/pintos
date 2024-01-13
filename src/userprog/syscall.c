b15dfcd342cdd76e314e5965bbbafb693fb3d100
Hanseul Jun <hanseuljun@gmail.com>
2024-01-12 19:16:55 -0800

    Working on userprog unit tests.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void syscall_exit (int status);
 
 void
 syscall_init (void) 
@@ -15,19 +16,29 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  // printf ("system call!\n");
+  /* 0x08084000 is the starting point of the code segment,
+     which is a very safe lower bound for detecting
+     bad memory access. */
+  if (f->esp < 0x08084000)
+    syscall_exit(-1);
+  /* 0xbffffffc is where the program arguments should be at.
+     0xbffffffc is chosen as a safe upper bound for detecting
+     bad memory access. */
+  if (f->esp >= 0xbffffffc)
+    syscall_exit(-1);
   int *arguments = f->esp;
   int number = arguments[0];
-  // printf ("syscall number: %d\n", number);
-  // printf ("thread name: %s\n", thread_name ());
 
   switch (number)
     {
       case SYS_EXIT:
         int status = arguments[1];
-        printf ("%s: exit(%d)\n", thread_name (), status);
-        thread_exit ();
+        syscall_exit (status);
         NOT_REACHED ();
+      case SYS_CREATE:
+        // TODO: Create file. Currently faking success.
+        f->eax = 0;
+        break;
       case SYS_WRITE:
         int fd = arguments[1];
         const void *buffer = (const void *) arguments[2];
@@ -37,4 +48,11 @@ syscall_handler (struct intr_frame *f)
           }
         break;
     }
+}
+
+static void syscall_exit (int status)
+{
+  printf ("%s: exit(%d)\n", thread_name (), status);
+  thread_exit ();
+  NOT_REACHED ();
 }
