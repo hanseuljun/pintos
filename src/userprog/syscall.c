5f558ea9fc2ec87373ab52402555ccc02a4c0b60
Hanseul Jun <hanseuljun@gmail.com>
2024-01-14 16:16:39 -0800

    Split exit with syscall_exit in userprog/syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,10 +7,12 @@
 #include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
-static void syscall_exit (int status);
-static bool syscall_create (const char *file, unsigned initial_size);
-static void syscall_write (int fd, void *buffer);
+static void syscall_exit (void *esp);
+static bool syscall_create (void *esp);
+static int syscall_open (void *esp);
+static void syscall_write (void *esp);
 static uint32_t get_argument (void *esp, size_t idx);
+static void exit(int status);
 
 void
 syscall_init (void) 
@@ -28,64 +30,77 @@ syscall_handler (struct intr_frame *f)
      address if it is pointing below here.
      See 3.1.4.1 Typical Memory Layout for the origin of 0x08048000. */
   if ((size_t)f->esp < 0x08048000)
-    syscall_exit (-1);
+    exit (-1);
   int number = (int) get_argument (f->esp, 0);
   // printf ("number: %d\n", number);
 
   switch (number)
     {
       case SYS_EXIT:
-        int status = (int) get_argument(f->esp, 1);
-        syscall_exit (status);
+        syscall_exit (f->esp);
         NOT_REACHED ();
-        break;
       case SYS_CREATE:
-        const char *file = (const char *) get_argument(f->esp, 1);
-        unsigned *initial_size = (unsigned) get_argument(f->esp, 2);
-        f->eax = syscall_create (file, initial_size);
-        break;
+        f->eax = syscall_create (f->esp);
+        return;
+      case SYS_OPEN:
+        f->eax = syscall_open (f->esp);
+        return;
       case SYS_WRITE:
-        int fd = (int) get_argument(f->esp, 1);
-        const void *buffer = (const void *)get_argument(f->esp, 2);
-        syscall_write (fd, buffer);
-        break;
+        syscall_write (f->esp);
+        return;
     }
-}
-
-static void syscall_exit (int status)
-{
-  printf ("%s: exit(%d)\n", thread_name (), status);
-  thread_exit ();
+  
+  printf ("Found syscall with a number (%d) not implemented yet.\n", number);
   NOT_REACHED ();
 }
 
-static bool syscall_create (const char *file, unsigned initial_size)
+static void syscall_exit (void *esp)
 {
+  int status = (int) get_argument(esp, 1);
+  exit (status);
+  NOT_REACHED ();
+}
+
+static bool syscall_create (void *esp)
+{
+  const char *file_name = (const char *) get_argument(esp, 1);
+  unsigned *initial_size = (unsigned) get_argument(esp, 2);
   struct thread *t = thread_current ();
 
-  /* Exit when file is pointing an invalid address. */
-  if (pagedir_get_page (t->pagedir, file) == NULL)
+  /* Exit when file_name is pointing an invalid address. */
+  if (pagedir_get_page (t->pagedir, file_name) == NULL)
     {
-      syscall_exit (-1);
+      exit (-1);
       NOT_REACHED ();
     }
-  /* Exit when file is NULL. */
-  if (file == NULL)
+  /* Exit when file_name is NULL. */
+  if (file_name == NULL)
     {
-      syscall_exit (-1);
+      exit (-1);
       NOT_REACHED ();
     }
-  /* Fail when file name is an empty string. */
-  if (file[0] == '\0')
+  /* Fail when file_name is an empty string. */
+  if (file_name[0] == '\0')
     {
       return false;
     }
-  // TODO: Create file. Currently faking success.
-  return filesys_create (file, initial_size);
+
+  return filesys_create (file_name, initial_size);
 }
 
-static void syscall_write (int fd, void *buffer)
+static int syscall_open (void *esp)
 {
+  const char *file_name = (const char *) get_argument(esp, 1);
+  struct file *file = filesys_open (file_name);
+  if (file == NULL)
+    return -1;
+  return file;
+}
+
+static void syscall_write (void *esp)
+{
+  int fd = (int) get_argument(esp, 1);
+  const void *buffer = (const void *)get_argument(esp, 2);
   if (fd == STDOUT_FILENO)
     {
       printf ((const char *) buffer);
@@ -99,8 +114,15 @@ static uint32_t get_argument (void *esp, size_t idx)
   // printf ("get_argument, addr: %p\n", addr);
   if (!is_user_vaddr (addr))
     {
-      syscall_exit (-1);
+      exit (-1);
       NOT_REACHED ();
     }
   return *addr;
+}
+
+static void exit (int status)
+{
+  printf ("%s: exit(%d)\n", thread_name (), status);
+  thread_exit ();
+  NOT_REACHED ();
 }
