612f396e2d66568f59952687cb0d62e389902642
Hanseul Jun <hanseuljun@gmail.com>
2024-01-16 18:19:54 -0800

    Rename functions in userprog/syscall and expose syscall_exit().

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -26,16 +26,16 @@ struct fd_info
 static struct fd_info *fd_info_map[FD_INFO_MAP_SIZE];
 
 static void syscall_handler (struct intr_frame *);
-static void syscall_exit (void *esp);
-static int syscall_exec (void *esp);
-static int syscall_wait (void *esp);
-static bool syscall_create (void *esp);
-static int syscall_open (void *esp);
-static int syscall_filesize (void *esp);
-static int syscall_read (void *esp);
-static int syscall_write (void *esp);
-static void syscall_seek (void *esp);
-static void syscall_close (void *esp);
+static void handle_exit (void *esp);
+static int handle_exec (void *esp);
+static int handle_wait (void *esp);
+static bool handle_create (void *esp);
+static int handle_open (void *esp);
+static int handle_filesize (void *esp);
+static int handle_read (void *esp);
+static int handle_write (void *esp);
+static void handle_seek (void *esp);
+static void handle_close (void *esp);
 static uint32_t get_argument (void *esp, size_t idx);
 static void exit(int status);
 static int find_available_fd (void);
@@ -46,6 +46,34 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void
+exit (int status)
+{
+  struct thread *t = thread_current ();
+  struct fd_info *fd_info;
+  int i;
+
+  /* Pass status to the waiter. */
+  if (t->exit_status_waiter)
+    *t->exit_status_waiter = status;
+
+  /* Close all files that belongs to the exiting process. */
+  for (i = 0; i < FD_INFO_MAP_SIZE; i++)
+    {
+      fd_info = fd_info_map[i];
+      if (fd_info != NULL && fd_info->pid == t->tid)
+        {
+          fd_info_map[i] = NULL;
+          file_close (fd_info->file);
+          free (fd_info);
+        }
+    }
+
+  printf ("%s: exit(%d)\n", thread_name (), status);
+  thread_exit ();
+  NOT_REACHED ();
 }
 
 static void
@@ -62,34 +90,34 @@ syscall_handler (struct intr_frame *f)
         shutdown_power_off ();
         NOT_REACHED ();
       case SYS_EXIT:
-        syscall_exit (f->esp);
+        handle_exit (f->esp);
         NOT_REACHED ();
       case SYS_EXEC:
-        f->eax = syscall_exec(f->esp);
+        f->eax = handle_exec(f->esp);
         return;
       case SYS_WAIT:
-        f->eax = syscall_wait(f->esp);
+        f->eax = handle_wait(f->esp);
         return;
       case SYS_CREATE:
-        f->eax = syscall_create (f->esp);
+        f->eax = handle_create (f->esp);
         return;
       case SYS_OPEN:
-        f->eax = syscall_open (f->esp);
+        f->eax = handle_open (f->esp);
         return;
       case SYS_FILESIZE:
-        f->eax = syscall_filesize (f->esp);
+        f->eax = handle_filesize (f->esp);
         return;
       case SYS_READ:
-        f->eax = syscall_read (f->esp);
+        f->eax = handle_read (f->esp);
         return;
       case SYS_WRITE:
-        f->eax = syscall_write (f->esp);
+        f->eax = handle_write (f->esp);
         return;
       case SYS_SEEK:
-        syscall_seek (f->esp);
+        handle_seek (f->esp);
         return;
       case SYS_CLOSE:
-        syscall_close (f->esp);
+        handle_close (f->esp);
         return;
     }
   
@@ -97,14 +125,16 @@ syscall_handler (struct intr_frame *f)
   NOT_REACHED ();
 }
 
-static void syscall_exit (void *esp)
+static void
+handle_exit (void *esp)
 {
   int status = (int) get_argument(esp, 1);
   exit (status);
   NOT_REACHED ();
 }
 
-static int syscall_exec (void *esp)
+static int
+handle_exec (void *esp)
 {
   const char *cmd_line = (const char *) get_argument(esp, 1);
   /* Exit when cmd_line is pointing an invalid address. */
@@ -116,13 +146,15 @@ static int syscall_exec (void *esp)
   return process_execute (cmd_line);
 }
 
-static int syscall_wait (void *esp)
+static int
+handle_wait (void *esp)
 {
   int pid = (int) get_argument(esp, 1);
   return process_wait (pid);
 }
 
-static bool syscall_create (void *esp)
+static bool
+handle_create (void *esp)
 {
   const char *file_name = (const char *) get_argument(esp, 1);
   unsigned initial_size = (unsigned) get_argument(esp, 2);
@@ -148,7 +180,8 @@ static bool syscall_create (void *esp)
   return filesys_create (file_name, initial_size);
 }
 
-static int syscall_open (void *esp)
+static int
+handle_open (void *esp)
 {
   const char *file_name = (const char *) get_argument(esp, 1);
   struct file *file;
@@ -181,14 +214,16 @@ static int syscall_open (void *esp)
   return fd;
 }
 
-static int syscall_filesize (void *esp)
+static int
+handle_filesize (void *esp)
 {
   int fd = (int) get_argument(esp, 1);
   struct fd_info *fd_info = fd_info_map[fd - FD_BASE];
   return file_length (fd_info->file);
 }
 
-static int syscall_read (void *esp)
+static int
+handle_read (void *esp)
 {
   int fd = (int) get_argument(esp, 1);
   void *buffer = (void *) get_argument(esp, 2);
@@ -211,7 +246,8 @@ static int syscall_read (void *esp)
   return file_read (fd_info->file, buffer, size);
 }
 
-static int syscall_write (void *esp)
+static int
+handle_write (void *esp)
 {
   int fd = (int) get_argument(esp, 1);
   const void *buffer = (const void *)get_argument(esp, 2);
@@ -237,7 +273,8 @@ static int syscall_write (void *esp)
   return file_write (fd_info->file, buffer, size);
 }
 
-static void syscall_seek (void *esp)
+static void
+handle_seek (void *esp)
 {
   int fd = (int) get_argument(esp, 1);
   unsigned position = (unsigned) get_argument(esp, 2);
@@ -251,7 +288,8 @@ static void syscall_seek (void *esp)
   file_seek (fd_info->file, position);
 }
 
-static void syscall_close (void *esp)
+static void
+handle_close (void *esp)
 {
   int fd = (int) get_argument(esp, 1);
   if (!is_fd_for_file (fd))
@@ -282,7 +320,8 @@ static void syscall_close (void *esp)
   free (fd_info);
 }
 
-static uint32_t get_argument (void *esp, size_t idx)
+static uint32_t
+get_argument (void *esp, size_t idx)
 {
   void *addr = esp + idx * sizeof(uint32_t);
   if (!is_uaddr_valid (addr))
@@ -293,34 +332,8 @@ static uint32_t get_argument (void *esp, size_t idx)
   return *(uint32_t *) addr;
 }
 
-static void exit (int status)
-{
-  struct thread *t = thread_current ();
-  struct fd_info *fd_info;
-  int i;
-
-  /* Pass status to the waiter. */
-  if (t->exit_status_waiter)
-    *t->exit_status_waiter = status;
-
-  /* Close all files that belongs to the exiting process. */
-  for (i = 0; i < FD_INFO_MAP_SIZE; i++)
-    {
-      fd_info = fd_info_map[i];
-      if (fd_info != NULL && fd_info->pid == t->tid)
-        {
-          fd_info_map[i] = NULL;
-          file_close (fd_info->file);
-          free (fd_info);
-        }
-    }
-
-  printf ("%s: exit(%d)\n", thread_name (), status);
-  thread_exit ();
-  NOT_REACHED ();
-}
-
-static int find_available_fd (void)
+static int
+find_available_fd (void)
 {
   int i;
   for (i = 0; i < FD_INFO_MAP_SIZE; i++)
@@ -334,23 +347,23 @@ static int find_available_fd (void)
 }
 
 /* Check validity of uaddr assuming its size is 4 bytes. */
-static bool is_uaddr_valid (const void *uaddr)
+static bool
+is_uaddr_valid (const void *uaddr)
 {
-  struct thread *t;
   uint32_t *pd;
 
   /* Examine the byte with the highest address. */
   if (!is_user_vaddr (uaddr + 3))
     return false;
 
-  t = thread_current ();
-  pd = t->pagedir;
+  pd = thread_current ()->pagedir;
   /* Examine bytes at the both ends of the 4-byte address. */
   return pagedir_get_page (pd, uaddr) != NULL
       && pagedir_get_page (pd, uaddr + 3) != NULL;
 }
 
-static bool is_fd_for_file (int fd)
+static bool
+is_fd_for_file (int fd)
 {
   return (fd >= FD_BASE) && (fd < (FD_BASE + FD_INFO_MAP_SIZE));
 }
