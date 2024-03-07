27bb6c42772007d93df72f5792bfeded73d3f44e
Hanseul Jun <hanseuljun@gmail.com>
2024-03-07 09:44:26 -0800

    Add comment for tackling page-linear.

--- src/vm/frame_table.c
+++ src/vm/frame_table.c
@@ -2,5 +2,10 @@
 
 void *frame_table_get_page (enum palloc_flags flags)
 {
-  return palloc_get_page (PAL_USER | flags);
+  void *page = palloc_get_page (PAL_USER | flags);
+  if (page != NULL)
+    return page;
+
+  // TODO: Implement swapping and pass page-linear. See 4.1.6 Managing the Swap Table.
+  return NULL;
 }
