e5beab59974546a76e3e7a5e15d5219ad4f2660d
Hanseul Jun <hanseuljun@gmail.com>
2024-03-10 12:40:01 -0700

    Pass pt-grow-pusha.

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -181,10 +181,11 @@ page_fault (struct intr_frame *f)
       NOT_REACHED ();
     }
 
-  /* Install a user virtual page for fault_addr when user is writing and the address belongs to user stack. */
+  /* Install a user virtual page for fault_addr when user is writing and the address belongs to user stack.
+     Gives additional 32 bytes of leeway to support stack growth by PUSHA. See 4.3.3 Stack Growth. */
   if (write
   && ((uint8_t *) fault_addr) < ((uint8_t *) PHYS_BASE)
-  && ((uint8_t *) fault_addr) >= ((uint8_t *) f->esp))
+  && ((uint8_t *) fault_addr) >= ((uint8_t *) f->esp - 32))
     {
       frame_table_install (pg_round_down (fault_addr), true);
       frame_table_set_stack_size (frame_table_get_stack_size() + PGSIZE);
