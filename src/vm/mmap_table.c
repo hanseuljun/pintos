20cfb35f673aa0238603f09e15ec3745ed57f7a8
Hanseul Jun <hanseuljun@gmail.com>
2024-04-07 20:02:30 -0700

    Improve mmap_table_update_file to pass mmap-unmap.

--- src/vm/mmap_table.c
+++ src/vm/mmap_table.c
@@ -1,9 +1,13 @@
 #include "mmap_table.h"
 #include <list.h>
+#include <stdio.h>
 #include "filesys/file.h"
+#include "userprog/pagedir.h"
 #include "threads/malloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "vm/frame_table.h"
+#include "vm/swap_table.h"
 
 struct mmap_elem
   {
@@ -11,7 +15,7 @@ struct mmap_elem
     struct file *file;
     int tid;
     void *uaddr;
-    size_t filesize;
+    int filesize;
     struct list_elem elem;
   };
 
@@ -19,7 +23,7 @@ static int next_mmap_id;
 static struct list mmap_list;
 
 static struct mmap_elem *mmap_table_find (void *uaddr);
-static struct mmap_elem *mmap_table_update_file (struct mmap_elem *mmap_elem);
+static void mmap_table_update_file (struct mmap_elem *mmap_elem);
 
 void mmap_table_init (void)
 {
@@ -43,6 +47,7 @@ void mmap_table_remove (int mapping)
 {
   struct list_elem *e;
 
+  printf ("mmap_table_remove - 1\n");
   e = list_begin (&mmap_list);
   while (e != list_end (&mmap_list))
     {
@@ -55,6 +60,7 @@ void mmap_table_remove (int mapping)
       e = list_remove (e);
       return;
     }
+  printf ("mmap_table_remove - 2\n");
 
   NOT_REACHED ();
 }
@@ -93,11 +99,29 @@ static struct mmap_elem *mmap_table_find (void *uaddr)
   return NULL;
 }
 
-struct mmap_elem *mmap_table_update_file (struct mmap_elem *mmap_elem)
+void mmap_table_update_file (struct mmap_elem *mmap_elem)
 {
-  void *uaddr = mmap_elem->uaddr;
+  struct thread *t = thread_current ();
+  uint32_t *pd = t->pagedir;
   for (int offset = 0; offset < mmap_elem->filesize; offset += PGSIZE)
     {
-      file_write_at (mmap_elem->file, uaddr + offset, PGSIZE, offset);
+      // Need to reinstall before starting to writing on file.
+      // Since writing requires acquiring the lock from the ata_disk's channel
+      // in ide_write, we need to do the obtaining beforehands.
+      // Otherwise, since obtaining also requires the lock by going through
+      // ide_read, there is a dead-lock.
+      // It is possible there was no access to some chunk of the file.
+      // In such case, there will be no page available for uaddr and also no
+      // corresponding file found from the swap table. Just move on in this case.
+      void *uaddr = mmap_elem->uaddr + offset;
+      if (pagedir_get_page (pd, uaddr) == NULL)
+        {
+          if (swap_table_find (t->tid, uaddr) != NULL)
+            frame_table_reinstall (uaddr);
+          else
+            continue;
+        }
+
+      file_write_at (mmap_elem->file, uaddr, PGSIZE, offset);
     }
 }
