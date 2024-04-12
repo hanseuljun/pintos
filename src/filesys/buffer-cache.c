0f2dfac99584a1d8e3718370600bdb87da1b1c53
Hanseul Jun <hanseuljun@gmail.com>
2024-04-11 22:13:57 -0700

    Add buffer_cache_elem.

--- src/filesys/buffer-cache.c
+++ src/filesys/buffer-cache.c
@@ -1,14 +1,26 @@
 #include "buffer-cache.h"
+#include <list.h>
 #include <string.h>
 #include "filesys/filesys.h"
 #include "threads/malloc.h"
 
-uint8_t *bounce;
+static uint8_t *bounce;
+static struct list buffer_list;
+
+struct buffer_cache_elem
+  {
+    uint8_t *buffer;
+    struct list_elem list_elem;
+  };
+
+struct buffer_cache_elem *buffer_cache_elem_create (void);
+void buffer_cache_elem_destroy (struct buffer_cache_elem *elem);
 
 void buffer_cache_init (void)
 {
   bounce = malloc (BLOCK_SECTOR_SIZE);
   ASSERT (bounce != NULL);
+  list_init (&buffer_list);
 }
 
 void buffer_cache_done (void)
@@ -21,6 +33,10 @@ uint8_t *buffer_cache_bounce (void)
   return bounce;
 }
 
+uint8_t *buffer_cache_buffer (block_sector_t sector_idx)
+{
+}
+
 void buffer_cache_read (block_sector_t sector_idx)
 {
   block_read (fs_device, sector_idx, bounce);
@@ -29,4 +45,17 @@ void buffer_cache_read (block_sector_t sector_idx)
 void buffer_cache_write (block_sector_t sector_idx)
 {
   block_write (fs_device, sector_idx, bounce);
+}
+
+struct buffer_cache_elem *buffer_cache_elem_create (void)
+{
+  struct buffer_cache_elem *elem = malloc (sizeof (*elem));
+  elem->buffer = malloc (BLOCK_SECTOR_SIZE);
+  return elem;
+}
+
+void buffer_cache_elem_destroy (struct buffer_cache_elem *elem)
+{
+  free (elem->buffer);
+  free (elem);
 }
--- src/filesys/buffer-cache.h
+++ src/filesys/buffer-cache.h
@@ -7,6 +7,7 @@
 void buffer_cache_init (void);
 void buffer_cache_done (void);
 uint8_t *buffer_cache_bounce (void);
+uint8_t *buffer_cache_buffer (block_sector_t sector_idx);
 void buffer_cache_read (block_sector_t sector_idx);
 void buffer_cache_write (block_sector_t sector_idx);
 
