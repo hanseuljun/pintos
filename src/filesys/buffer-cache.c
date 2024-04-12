f250a687eb07f4de7d65e3cc0cf44fba5f950790
Hanseul Jun <hanseuljun@gmail.com>
2024-04-11 22:04:48 -0700

    Rename buffer to bounce in buffer-cache.

--- src/filesys/buffer-cache.c
+++ src/filesys/buffer-cache.c
@@ -3,30 +3,30 @@
 #include "filesys/filesys.h"
 #include "threads/malloc.h"
 
-uint8_t *buffer;
+uint8_t *bounce;
 
 void buffer_cache_init (void)
 {
-  buffer = malloc (BLOCK_SECTOR_SIZE);
-  ASSERT (buffer != NULL);
+  bounce = malloc (BLOCK_SECTOR_SIZE);
+  ASSERT (bounce != NULL);
 }
 
 void buffer_cache_done (void)
 {
-  free (buffer);
+  free (bounce);
 }
 
-uint8_t *buffer_cache_buffer (void)
+uint8_t *buffer_cache_bounce (void)
 {
-  return buffer;
+  return bounce;
 }
 
 void buffer_cache_read (block_sector_t sector_idx)
 {
-  block_read (fs_device, sector_idx, buffer);
+  block_read (fs_device, sector_idx, bounce);
 }
 
 void buffer_cache_write (block_sector_t sector_idx)
 {
-  block_write (fs_device, sector_idx, buffer);
+  block_write (fs_device, sector_idx, bounce);
 }
