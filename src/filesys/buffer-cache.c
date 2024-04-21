44b8af7dcaa94cc7bbc06a2666cd0b102155b6ac
Hanseul Jun <hanseuljun@gmail.com>
2024-04-20 18:14:34 -0700

    Replace bounce with a single element list.

--- src/filesys/buffer-cache.c
+++ src/filesys/buffer-cache.c
@@ -8,7 +8,6 @@
 /* From 5.3.4 Buffer Cache. */
 #define MAX_BUFFER_LIST_SIZE 64
 
-static uint8_t *bounce;
 static struct lock buffer_lock;
 static struct list buffer_list;
 
@@ -23,15 +22,17 @@ void buffer_cache_elem_destroy (struct buffer_cache_elem *elem);
 
 void buffer_cache_init (void)
 {
-  bounce = malloc (BLOCK_SECTOR_SIZE);
-  ASSERT (bounce != NULL);
   lock_init (&buffer_lock);
   list_init (&buffer_list);
+  struct buffer_cache_elem *elem = buffer_cache_elem_create ();
+  list_push_back (&buffer_list, &elem->list_elem);
 }
 
 void buffer_cache_done (void)
 {
-  free (bounce);
+  struct list_elem *e = list_pop_front (&buffer_list);
+  struct buffer_cache_elem *elem = list_entry (e, struct buffer_cache_elem, list_elem);
+  buffer_cache_elem_destroy (elem);
 }
 
 struct lock *buffer_cache_get_lock (void)
@@ -41,19 +42,25 @@ struct lock *buffer_cache_get_lock (void)
 
 uint8_t *buffer_cache_get_buffer (block_sector_t sector_idx)
 {
-  return bounce;
+  struct list_elem *e = list_front (&buffer_list);
+  struct buffer_cache_elem *elem = list_entry (e, struct buffer_cache_elem, list_elem);
+  return elem->buffer;
 }
 
 void buffer_cache_read (block_sector_t sector_idx)
 {
   // TODO: Save in buffer_list, bounce, in a way clients should use
   // buffer_cache_buffer, instead of buffer_cache_bounce.
-  block_read (fs_device, sector_idx, bounce);
+  struct list_elem *e = list_front (&buffer_list);
+  struct buffer_cache_elem *elem = list_entry (e, struct buffer_cache_elem, list_elem);
+  block_read (fs_device, sector_idx, elem->buffer);
 }
 
 void buffer_cache_write (block_sector_t sector_idx)
 {
-  block_write (fs_device, sector_idx, bounce);
+  struct list_elem *e = list_front (&buffer_list);
+  struct buffer_cache_elem *elem = list_entry (e, struct buffer_cache_elem, list_elem);
+  block_write (fs_device, sector_idx, elem->buffer);
 }
 
 struct buffer_cache_elem *buffer_cache_elem_create (void)
