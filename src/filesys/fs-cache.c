cf94e1eb440d6cda30f37a58750a09932910da2d
Hanseul Jun <hanseuljun@gmail.com>
2024-04-21 10:20:32 -0700

    Use multiple elements in fs-cache.

--- src/filesys/fs-cache.c
+++ src/filesys/fs-cache.c
@@ -13,19 +13,20 @@ static struct list buffer_list;
 
 struct fs_cache_elem
   {
+    block_sector_t sector_idx;
     uint8_t *buffer;
     struct list_elem list_elem;
   };
 
-struct fs_cache_elem *fs_cache_elem_create (void);
+struct fs_cache_elem *fs_cache_find (block_sector_t sector_idx);
+
+struct fs_cache_elem *fs_cache_elem_create (block_sector_t sector_idx);
 void fs_cache_elem_destroy (struct fs_cache_elem *elem);
 
 void fs_cache_init (void)
 {
   lock_init (&buffer_lock);
   list_init (&buffer_list);
-  struct fs_cache_elem *elem = fs_cache_elem_create ();
-  list_push_back (&buffer_list, &elem->list_elem);
 }
 
 void fs_cache_done (void)
@@ -42,31 +43,87 @@ struct lock *fs_cache_get_lock (void)
 
 uint8_t *fs_cache_get_buffer (block_sector_t sector_idx)
 {
-  struct list_elem *e = list_front (&buffer_list);
-  struct fs_cache_elem *elem = list_entry (e, struct fs_cache_elem, list_elem);
+  struct fs_cache_elem *elem = fs_cache_find (sector_idx);
+  if (elem == NULL)
+    {
+      if (list_size (&buffer_list) < MAX_BUFFER_LIST_SIZE)
+        {
+          elem = fs_cache_elem_create (sector_idx);
+        }
+      else
+        {
+          struct list_elem *e = list_pop_front (&buffer_list);
+          elem = list_entry (e, struct fs_cache_elem, list_elem);
+          elem->sector_idx = sector_idx;
+        }
+      list_push_back (&buffer_list, &elem->list_elem);
+    }
+
   return elem->buffer;
 }
 
 void fs_cache_read (block_sector_t sector_idx)
 {
-  // TODO: Save in buffer_list, bounce, in a way clients should use
-  // buffer_cache_buffer, instead of buffer_cache_bounce.
-  struct list_elem *e = list_front (&buffer_list);
-  struct fs_cache_elem *elem = list_entry (e, struct fs_cache_elem, list_elem);
+  struct fs_cache_elem *elem = fs_cache_find (sector_idx);
+  if (elem == NULL)
+    {
+      if (list_size (&buffer_list) < MAX_BUFFER_LIST_SIZE)
+        {
+          elem = fs_cache_elem_create (sector_idx);
+        }
+      else
+        {
+          struct list_elem *e = list_pop_front (&buffer_list);
+          elem = list_entry (e, struct fs_cache_elem, list_elem);
+          elem->sector_idx = sector_idx;
+        }
+      list_push_back (&buffer_list, &elem->list_elem);
+    }
+
   block_read (fs_device, sector_idx, elem->buffer);
 }
 
 void fs_cache_write (block_sector_t sector_idx)
 {
-  struct list_elem *e = list_front (&buffer_list);
-  struct fs_cache_elem *elem = list_entry (e, struct fs_cache_elem, list_elem);
+  struct fs_cache_elem *elem = fs_cache_find (sector_idx);
+  if (elem == NULL)
+    {
+      if (list_size (&buffer_list) < MAX_BUFFER_LIST_SIZE)
+        {
+          elem = fs_cache_elem_create (sector_idx);
+        }
+      else
+        {
+          struct list_elem *e = list_pop_front (&buffer_list);
+          elem = list_entry (e, struct fs_cache_elem, list_elem);
+          elem->sector_idx = sector_idx;
+        }
+      list_push_back (&buffer_list, &elem->list_elem);
+      block_read (fs_device, sector_idx, elem->buffer);
+    }
+
   block_write (fs_device, sector_idx, elem->buffer);
 }
 
-struct fs_cache_elem *fs_cache_elem_create (void)
+struct fs_cache_elem *fs_cache_find (block_sector_t sector_idx)
+{
+  struct list_elem *e;
+
+  for (e = list_begin (&buffer_list); e != list_end (&buffer_list);
+       e = list_next (e))
+    {
+      struct fs_cache_elem *elem = list_entry (e, struct fs_cache_elem, list_elem);
+      if (elem->sector_idx == sector_idx)
+        return elem;
+    }
+  return NULL;
+}
+
+struct fs_cache_elem *fs_cache_elem_create (block_sector_t sector_idx)
 {
   struct fs_cache_elem *elem = malloc (sizeof (*elem));
   elem->buffer = malloc (BLOCK_SECTOR_SIZE);
+  elem->sector_idx = sector_idx;
   return elem;
 }
 
