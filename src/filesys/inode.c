bf618cc7067fdfdfa4f25d8b3d5a628ef15a22ee
Hanseul Jun <hanseuljun@gmail.com>
2024-05-29 18:35:07 -0700

    Add inode_data.c/h.

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -5,44 +5,10 @@
 #include <string.h>
 #include "filesys/fs-cache.h"
 #include "filesys/free-map.h"
+#include "filesys/inode_data.h"
 #include "threads/malloc.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
-
-/* Identifies an inode. */
-#define INODE_MAGIC 0x494e4f44
-#define INODE_DISK_MAX_SECTOR_COUNT 118
-#define INVALID_SECTOR UINT32_MAX
-
-/* On-disk inode.
-   Must be exactly BLOCK_SECTOR_SIZE bytes long. */
-struct direct_inode_disk
-  {
-    block_sector_t sectors[INODE_DISK_MAX_SECTOR_COUNT];  /* First data sector. */
-    off_t length;                       /* File size in bytes. */
-    block_sector_t indirect_sector;
-    unsigned magic;                     /* Magic number. */
-    uint32_t unused[7];                 /* Not used. */
-  };
-
-struct indirect_inode_disk
-  {
-    block_sector_t sectors[INODE_DISK_MAX_SECTOR_COUNT];  /* First data sector. */
-    unsigned magic;                     /* Magic number. */
-    uint32_t unused[9];                 /* Not used. */
-  };
-
-struct inode_sector_counts
-  {
-    size_t direct_sector_count;
-    size_t indirect_sector_count;
-  };
-
-struct inode_data
-  {
-    struct direct_inode_disk direct_inode_disk;
-    struct indirect_inode_disk indirect_inode_disk;
-  };
 
 /* Returns the number of sectors to allocate for an inode SIZE
    bytes long. */
---
+++ src/filesys/inode_data.h
@@ -0,0 +1,39 @@
+#ifndef FILESYS_INODE_DATA_H
+#define FILESYS_INODE_DATA_H
+
+/* Identifies an inode. */
+#define INODE_MAGIC 0x494e4f44
+#define INODE_DISK_MAX_SECTOR_COUNT 118
+#define INVALID_SECTOR UINT32_MAX
+
+/* On-disk inode.
+   Must be exactly BLOCK_SECTOR_SIZE bytes long. */
+struct direct_inode_disk
+  {
+    block_sector_t sectors[INODE_DISK_MAX_SECTOR_COUNT];  /* First data sector. */
+    off_t length;                       /* File size in bytes. */
+    block_sector_t indirect_sector;
+    unsigned magic;                     /* Magic number. */
+    uint32_t unused[7];                 /* Not used. */
+  };
+
+struct indirect_inode_disk
+  {
+    block_sector_t sectors[INODE_DISK_MAX_SECTOR_COUNT];  /* First data sector. */
+    unsigned magic;                     /* Magic number. */
+    uint32_t unused[9];                 /* Not used. */
+  };
+
+struct inode_sector_counts
+  {
+    size_t direct_sector_count;
+    size_t indirect_sector_count;
+  };
+
+struct inode_data
+  {
+    struct direct_inode_disk direct_inode_disk;
+    struct indirect_inode_disk indirect_inode_disk;
+  };
+
+#endif /* filesys/inode_data.h */
